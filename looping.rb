db885dfcd457157bc082e77be0093eb3303444f0
alfred kimata <alfyjason@gmail.com>
2023-07-06 12:09:34 +0300

    control flow lab complete

--- looping.rb
+++ looping.rb
@@ -1,5 +1,12 @@
 def happy_new_year
   # your code here
+  counter = 10;
+  while counter > 0
+    puts counter
+    counter -= 1
+  end
+  puts "Happy New Year!"
+
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +24,18 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |num|
+    puts fizzbuzz(num)
+  end
 end
 
 def reverse_string(str)
   # your code here
+  reversed = ""
+  index = str.length - 1
+  while index >= 0
+    reversed += str[index]
+    index -= 1
+  end
+  reversed
 end
